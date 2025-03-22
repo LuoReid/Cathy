201e4658fc6f85a7794e8cebe009de4ea8295039
luo.reid <luo.reid@gmail.com>
2025-03-22 13:14:06 +0800

    Preload

---
+++ renderer.js
@@ -0,0 +1,10 @@
+const info = document.getElementById('info')
+info.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`
+
+const ping = async () => {
+  const response = await window.versions.ping()
+
+  console.log(response)
+}
+
+ping()
